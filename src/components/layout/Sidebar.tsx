8db27f48dbc73eac28c80ce6fc1eaba4cc10e308
juan patricio doyle <juanpatriciodoyle@gmail.com>
2025-06-12 13:45:31 +0200

    Fix active

--- src/components/layout/Sidebar.tsx
+++ src/components/layout/Sidebar.tsx
@@ -72,7 +72,7 @@ const StyledNavLink = styled(NavLink)`
         }
     }
 
-    &:active {
+    &.active {
         background-image: linear-gradient(to right, #4f46e5, #6366f1);
         color: #ffffff;
         box-shadow: 0 4px 6px -1px rgba(0, 0, 0, 0.1), 0 2px 4px -1px rgba(0, 0, 0, 0.06);
